b1adf147878050e30d214627105d2acdd8583c7e
bronzo97 <danilobronzo97@hotmail.it>
2022-07-01 17:53:51 +0200

    started footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,6 +1,74 @@
 <template>
     <div>
-
+        <footer>
+        <div class="footer-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col">                    
+                        <ul>
+                            <li><img src="" alt=""></li>
+                            <li><p>A funtional HTML Template for corporate & business.</p></li>
+                            <li><i class="fa-solid fa-phone"></i> +1 (305) 1234-5678</li>
+                            <li><i class="fa-solid fa-envelope"></i> hello@example.com</li>
+                            <li><i class="fa-solid fa-location-dot"></i> Main Avenue, 987</li>
+                            <li><div class="btn btn primary">Get in touch</div></li>
+                        </ul>                    
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <h5>About</h5>
+                            <ul>
+                                <li><i class="fa-solid fa-angle-right"></i>The Company</li>
+                                <li><i class="fa-solid fa-angle-right"></i>Institutional</li>
+                                <li><i class="fa-solid fa-angle-right"></i>Social & Events</li>
+                                <li><i class="fa-solid fa-angle-right"></i>Innovation</li>
+                                <li><i class="fa-solid fa-angle-right"></i>Environment</li>
+                                <li><i class="fa-solid fa-angle-right"></i>Technology</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <h5>Services</h5>
+                            <ul>
+                                <li><i class="fa-solid fa-angle-right"></i>Audit & Assurance</li>
+                                <li><i class="fa-solid fa-angle-right"></i>Financial Advisory</li>
+                                <li><i class="fa-solid fa-angle-right"></i>Analytics M&A</li>
+                                <li><i class="fa-solid fa-angle-right"></i>Middle Marketing</li>
+                                <li><i class="fa-solid fa-angle-right"></i>Legal Consulting</li>
+                                <li><i class="fa-solid fa-angle-right"></i>Regulatory Risk</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <h5>Support</h5>
+                            <ul>
+                                <li><i class="fa-solid fa-angle-right"></i>Responsibility</li>
+                                <li><i class="fa-solid fa-angle-right"></i>Terms of Use</li>
+                                <li><i class="fa-solid fa-angle-right"></i>About Cookies</li>
+                                <li><i class="fa-solid fa-angle-right"></i>Privacy Policy</li>
+                                <li><i class="fa-solid fa-angle-right"></i>Accessibility</li>
+                                <li><i class="fa-solid fa-angle-right"></i>Information</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-credits">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div>Enjoy the low price. We are tracking any intention of piracy</div>
+                    </div>
+                    <div class="col">
+                        <div><i class="fa-regular fa-copyright"></i> 2020 NEXGEN is Proudly Powered by Codings</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
     </div>
 </template>
 
@@ -12,6 +80,14 @@
 
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    footer {
+        height: 100vh;
+        .footer-bg{
+        background-image: url('../../public/images/bg-7.jpg');
+        height: 100%;
+        background-size: cover;
+        }
+    }
 
 </style>
