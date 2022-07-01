227d3ec8cc911a1fd73d0df12b9db32b29a3b3a7
bronzo97 <danilobronzo97@hotmail.it>
2022-07-01 12:54:15 +0200

    import img/bs/fa/gf

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import "./assets/scss/main.scss"
+
 Vue.config.productionTip = false
 
 new Vue({
