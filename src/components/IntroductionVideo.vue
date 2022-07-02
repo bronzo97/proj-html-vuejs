43b0551fa532e479ae7de9c9790b0fcd813c98de
bronzo97 <danilobronzo97@hotmail.it>
2022-07-02 16:40:50 +0200

    creted new other components/ start video component

---
+++ src/components/IntroductionVideo.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="video-section">
+        <div class="container">
+            <div class="row text-center justify-content-center align-items-center">
+                <div class="col col-5 text-center">
+                    <p>introduction video</p>
+                    <h2>Advisory Preformance</h2>
+                    <p>Financial executives need to review their strategies, operating models and capabilities so that their areas can generate sound informations for better decision making.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="video-img">
+                        <div class="play"><i class="fa-solid fa-play"></i></div>
+                        <div class="play"><i class="fa-solid fa-play"></i></div>
+                    </div>
+                </div>
+            </div>        
+                
+            
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'IntroductionVideo',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+.video-section {
+
+
+    .video-img {
+        width: 1000px;
+        height: 400px;
+        background-image: url('../../public/images/video-1.jpg');
+        background-position: center;
+        background-size: cover;
+
+    }
+}
+    
+</style>
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,13 +1,37 @@
 <template>
     <div>
-
+        <IntroductionVideo></IntroductionVideo>
+        <ResultsInNumber></ResultsInNumber>
+        <OurBusinessArea></OurBusinessArea>
+        <TheTeam></TheTeam>
+        <LatestNews></LatestNews>
+        <TheNewsLetter></TheNewsLetter>
+        <TheForm></TheForm>
     </div>
 </template>
 
 <script>
+    import IntroductionVideo from './IntroductionVideo.vue'
+    import ResultsInNumber from './ResultsInNumber.vue'
+    import OurBusinessArea from './OurBusinessArea.vue'
+    import TheTeam from './TheTeam.vue'
+    import LatestNews from './LatestNews.vue'
+    import TheNewsLetter from './TheNewsLetter.vue'
+    import TheForm from './TheForm.vue'
+
+
     export default {
         name: 'TheMain',
-        
+        components: {
+            IntroductionVideo,
+            ResultsInNumber,
+            OurBusinessArea,
+            TheTeam,
+            LatestNews,
+            TheNewsLetter,
+            TheForm,
+    }
+
 }
 
 </script>
