bbdfe98aca03961191c45bc8c4143a0713db555f
bronzo97 <danilobronzo97@hotmail.it>
2022-07-02 14:23:52 +0200

    styled footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -3,21 +3,21 @@
         <footer>
         <div class="footer-bg">
             <div class="container">
-                <div class="row">
+                <div class="row row-cols-4 row-cols-lg-5 justify-content-center">
                     <div class="col">                    
-                        <ul>
+                        <ul class="list-unstyled">
                             <li><img src="" alt=""></li>
                             <li><p>A funtional HTML Template for corporate & business.</p></li>
                             <li><i class="fa-solid fa-phone"></i> +1 (305) 1234-5678</li>
                             <li><i class="fa-solid fa-envelope"></i> hello@example.com</li>
                             <li><i class="fa-solid fa-location-dot"></i> Main Avenue, 987</li>
-                            <li><div class="btn btn primary">Get in touch</div></li>
+                            <li class="text-center mt-4"><div class="btn btn-primary">Get in touch</div></li>
                         </ul>                    
                     </div>
                     <div class="col">
                         <div class="card">
                             <h5>About</h5>
-                            <ul>
+                            <ul class="list-unstyled">
                                 <li><i class="fa-solid fa-angle-right"></i>The Company</li>
                                 <li><i class="fa-solid fa-angle-right"></i>Institutional</li>
                                 <li><i class="fa-solid fa-angle-right"></i>Social & Events</li>
@@ -30,7 +30,7 @@
                     <div class="col">
                         <div class="card">
                             <h5>Services</h5>
-                            <ul>
+                            <ul class="list-unstyled">
                                 <li><i class="fa-solid fa-angle-right"></i>Audit & Assurance</li>
                                 <li><i class="fa-solid fa-angle-right"></i>Financial Advisory</li>
                                 <li><i class="fa-solid fa-angle-right"></i>Analytics M&A</li>
@@ -43,7 +43,7 @@
                     <div class="col">
                         <div class="card">
                             <h5>Support</h5>
-                            <ul>
+                            <ul class="list-unstyled">
                                 <li><i class="fa-solid fa-angle-right"></i>Responsibility</li>
                                 <li><i class="fa-solid fa-angle-right"></i>Terms of Use</li>
                                 <li><i class="fa-solid fa-angle-right"></i>About Cookies</li>
@@ -58,12 +58,12 @@
         </div>
         <div class="footer-credits">
             <div class="container">
-                <div class="row">
+                <div class="row footer-credits">
                     <div class="col">
                         <div>Enjoy the low price. We are tracking any intention of piracy</div>
                     </div>
-                    <div class="col">
-                        <div><i class="fa-regular fa-copyright"></i> 2020 NEXGEN is Proudly Powered by Codings</div>
+                    <div class="col d-flex justify-content-end">
+                        <div><i class="fa-regular fa-copyright"></i> 2020 NEXGEN is Proudly Powered by <span class="codings-color">Codings</span>.</div>
                     </div>
                 </div>
             </div>
@@ -82,12 +82,45 @@
 
 <style lang="scss" scoped>
     footer {
-        height: 100vh;
         .footer-bg{
         background-image: url('../../public/images/bg-7.jpg');
+        background-position: center;
         height: 100%;
         background-size: cover;
+        padding: 100px 0;
+        color: #fff;
+
+
+        i {
+            margin: 0.5rem;
         }
+
+        h5 {
+            padding: 0.4rem;
+        }
+        }
+
+        .card {
+            background-color: #4e4f5244;
+            
+            color: #e7eee9;
+            padding: 1rem;
+            
+            li {
+                margin: 0 0.3rem;
+            }
+        }
+    .footer-credits {
+        background-color: #050A0D;
+        padding: 1rem;
+        color: #e7eee9;
+        font-size: 0.8rem;
+        
+        .codings-color {
+        color: #048383
+    }
+    }
+    
     }
 
 </style>
