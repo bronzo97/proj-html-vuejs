9c129d333a95af2ab369b698576410542ee72640
bronzo97 <danilobronzo97@hotmail.it>
2022-07-03 08:51:07 +0200

    OurBusinessAres done

---
+++ src/components/OurBusinessArea.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="our-business-area-section">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <p>Our business area</p>
+                    <h2>Excellence in Service</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col col-8 d-flex">
+                    <p>we are leaders in providing consultancy services with a set of cutting-edge technologies and a team of experienced and ronowned professionals. These are somo options that you can hire.</p>
+                </div>  
+                <div class="col col-4 text-end">  
+                    <div class="btn btn-primary">See all</div>                    
+                </div>
+            </div>
+            <div class="row row-cols-6">
+                <div class="col col-4">
+                    <div class="card">
+                        <div class="my-card-header"><i class="fa-solid fa-network-wired"></i><i class="fa-solid fa-arrow-right"></i></div>
+                        <div class="card-body">
+                            <h5 class="card-title">Audit & Assurance</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col col-4">
+                    <div class="card">
+                        <div class="my-card-header"><i class="fa-solid fa-network-wired"></i><i class="fa-solid fa-arrow-right"></i></div>
+                        <div class="card-body">
+                            <h5 class="card-title">Audit & Assurance</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col col-4">
+                    <div class="card">
+                        <div class="my-card-header"><i class="fa-solid fa-network-wired"></i><i class="fa-solid fa-arrow-right"></i></div>
+                        <div class="card-body">
+                            <h5 class="card-title">Audit & Assurance</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col col-4">
+                    <div class="card">
+                        <div class="my-card-header"><i class="fa-solid fa-network-wired"></i><i class="fa-solid fa-arrow-right"></i></div>
+                        <div class="card-body">
+                            <h5 class="card-title">Audit & Assurance</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col col-4">
+                    <div class="card">
+                        <div class="my-card-header"><i class="fa-solid fa-network-wired"></i><i class="fa-solid fa-arrow-right"></i></div>
+                        <div class="card-body">
+                            <h5 class="card-title">Audit & Assurance</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col col-4">
+                    <div class="card">
+                        <div class="my-card-header"><i class="fa-solid fa-network-wired"></i><i class="fa-solid fa-arrow-right"></i></div>
+                        <div class="card-body">
+                            <h5 class="card-title">Audit & Assurance</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'OurBusinessArea',
+    }
+</script>
+
+<style lang="scss" scoped>
+    .our-business-area-section {
+        background-color: #111116;
+        color: #fff;
+        .my-card-header {
+            padding: 1rem 1rem 0 1rem;
+            display: flex;
+            justify-content: space-between;
+            
+        }
+
+        .card {
+            margin: .5rem;
+            background-color: #191920;
+
+        }
+
+        i:first-child {
+            font-size: 2rem;
+        }
+        i:last-child {
+            font-size: 1.5rem;
+        }
+    }
+</style>
